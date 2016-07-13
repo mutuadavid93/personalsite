e9648bb09abe026784455b2b6a2fda386e0f7ae8
Street Money <Street Money>
2016-07-13 11:54:23 +0300

    basic scaffold for website finish

--- register.php
+++ register.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link href='https://fonts.googleapis.com/css?family=Bree+Serif|Merriweather:400,300,300italic,400italic,700,700italic' rel='stylesheet' type='text/css'>
     <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Bootstrap Workfolows ---Home</title>
+    <title>Bootstrap Workfolows --- Register</title>
     <!-- Bootstrap -->
     <link href="_/css/bootstrap.css" rel="stylesheet">
     <link href="_/css/mystyles.css" rel="stylesheet">
